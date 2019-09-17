7c79a9e95a5cbb9f042df806269ef61969089f5d
JorgeAntonGarcia <54888053+JorgeAntonGarcia@users.noreply.github.com>
2019-09-17 15:29:01 +0200

    Add files via upload

---
+++ AA_Project/AA_Project/argument.h
@@ -0,0 +1,26 @@
+#ifndef ARGUMENT_H_
+#define ARGUMENT_H_
+
+enum NATURE_SET { ECONOMIC = 1, ECOLOGIC, SOCIAL };
+
+class argument
+{
+private:
+	int nature; 
+	bool is_pro; // PRO = TRUE, CON = FALSE
+	float strenght; // THE STRENGHT OF THE ARGUMENT IN THE FIELD
+	float validity;// HOW MUCH AN ARGUMENT IS CONSISTENT
+
+public:
+	argument() { nature = ECONOMIC; is_pro = true; strenght = 0.0; validity = 0.0; };
+	argument(int NAT = ECONOMIC, bool PRO = true, float STR = 0.0, float VAL = 0.0) { nature = NAT;  is_pro = PRO; 
+	strenght = STR; validity = VAL; };
+	~argument() {};
+
+	//SET AND GET FUNCTIONS
+	void Set_nature(int NAT) { nature = NAT; };		  int Get_nature() { return nature; };
+	void Set_strenght(float STR) { strenght = STR; }; float Get_strenght() { return strenght; };
+	void Set_validity(float VAL) { validity = VAL; }; float Get_validity() { return validity; };
+};
+
+#endif // !1
---
+++ AA_Project/AA_Project/listener.h
@@ -0,0 +1,39 @@
+#ifndef LISTENER_H_
+#define LISTENER_H_
+
+#include "argument.h"
+#include <list>
+
+using namespace std;
+
+class listener {
+private:
+	// ARGUMENTS THAT A LISTENER HAS
+	list <argument> argument_list;
+	// PARAMETERS FOR THE SCORE AND RESULTS
+	float economic_opinion;
+	float ecologic_opinion;
+	float social_opinion;
+	// GRADE OF TOLERANCE TO ACCEPT NEW ARGUMENTS
+	float economic_acep;
+	float ecologic_acep;
+	float social_acep;
+	// GRADE OF GRADDING THE DIFFERENT SUBJECTS
+	float economic_value;
+	float ecologic_value;
+	float social_value;
+
+public:
+	listener() {};
+	~listener() {};
+
+	// FUNCTION OF ACCEPTANCE OF NEW ARGUMENTS, RETURNING A BOOLEAN (TRUE = ACCEPTED, FALSE = REJECTED)
+	bool Add_argument(argument Arg) { 
+		bool accepted = false;
+		// CREATE THE FUNCTION OF CHECKING THE ARGUMENT
+		return accepted; 
+	}
+
+}
+
+#endif
